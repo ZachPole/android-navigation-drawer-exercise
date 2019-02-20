6804cc38b53706e9e9dce2a9c26e69b83ca6f3d3
Zach Pole <42594071+ZachPole@users.noreply.github.com>
2019-02-20 14:51:08 -0800

    initial upload of lab

---
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
@@ -0,0 +1,123 @@
+package com.codepath.android.navigationdrawerexercise.activities;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.codepath.android.navigationdrawerexercise.R;
+import com.codepath.android.navigationdrawerexercise.fragments.FamilyGuyFragment;
+import com.codepath.android.navigationdrawerexercise.fragments.FuturamaFragment;
+import com.codepath.android.navigationdrawerexercise.fragments.SimpsonsFragment;
+import com.codepath.android.navigationdrawerexercise.fragments.SouthParkFragment;
+
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+
+        //Find out drawer view
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        drawerToggle = setupDrawerToggle();
+        setupDrawerContent(nvDrawer);
+    }
+    
+    private ActionBarDrawerToggle setupDrawerToggle(){
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+       // switch (item.getItemId()){
+        //    case android.R.id.home:
+          //      mDrawer.openDrawer(GravityCompat.START);
+            //    return true;
+       // }
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+   private void setupDrawerContent(NavigationView navigationView){
+        navigationView.setNavigationItemSelectedListener(
+               new NavigationView.OnNavigationItemSelectedListener() {
+                   @Override
+                   public boolean onNavigationItemSelected(MenuItem menuItem) {
+                       selectDrawerItem(menuItem);
+                       return true;
+                   }
+               });
+   }
+
+   public void selectDrawerItem(MenuItem menuItem){
+        Fragment fragment = null;
+        Class fragmentClass = null;
+        switch (menuItem.getItemId()){
+            case R.id.FamilyGuy:
+                fragmentClass = FamilyGuyFragment.class;
+                break;
+            case R.id.Futurama:
+                fragmentClass = FuturamaFragment.class;
+                break;
+            case R.id.SouthPark:
+                fragmentClass = SouthParkFragment.class;
+                break;
+            case R.id.TheSimpsons:
+                fragmentClass = SimpsonsFragment.class;
+                break;
+
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+       FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawer.closeDrawers();
+   }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+}
